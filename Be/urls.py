6878b6d8fd3cd843e61fb40b357f85e218092142
Pavan0303 <pawanangajala812@gmail.com>
2024-11-27 15:44:30 +0530

    first commit

---
+++ Be/urls.py
@@ -0,0 +1,7 @@
+from Be.views import *
+from django.urls import path 
+
+
+urlpatterns = [
+    path('index/',index,name='index'),
+]
---
+++ Btech/urls.py
@@ -0,0 +1,6 @@
+from Btech.views import *
+from django.urls import path
+
+urlpatterns=[
+    path('index/',index,name='index')
+]
